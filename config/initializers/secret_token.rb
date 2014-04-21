bc3a26253e9a4c82a621ad736b6e49148c2c6239
ServerJunge <serverjunge@live.de>
2014-04-21 17:26:11 +0200

    Installation of ServerJunge
    Just the Installation without any Code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Serverjunge::Application.config.secret_key_base = '90ec1b2f092d92f6d16d8d82050dca463cb33c9ce7ae70e992e62baf719af282800b9ee70744c8219afaa01561396de2881f73fbd4fc358c64f5405af27c25c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Serverjunge::Application.config.session_store :cookie_store, key: '_serverjunge_session'
