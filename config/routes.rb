88c1c036a0e45bf3d34c1255646d6743ce69048f
ServerJunge <serverjunge@live.de>
2014-04-28 18:56:11 +0200

    Temp Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Serverjunge::Application.routes.draw do
 
   devise_for :users
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
+
   
  	get 'tags/:tag', to: 'articles#index', as: :tag
   resources :articles
